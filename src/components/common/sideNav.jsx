002113460e4a63309029f15e2b29ae08d6ff13ce
dev-phantom <godsfavour1975@gmail.com>
2023-04-16 12:39:35 +0100

    done 80% of the stuff

---
+++ src/components/common/sideNav.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Link from 'next/link';
+import Image from "next/image";
+
+
+export default function SideNav() {
+  let links = [
+    { name: "Overview", link: "/#", icon:"https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/Group_3_m9874k.png" },
+    { name: "Accounts", link: "/#", icon:"https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/Group_2_c1smgx.png"},
+    { name: "Transfers", link: "/#", icon:"https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/Group_1_2_jucj8w.png" },
+    { name: "Statements", link: "/#", icon:"https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/Group_3_m9874k.png" },
+    { name: "Transactions", link: "/#", icon:"https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/Group_4_bbrs70.png" },
+    { name: "Settings", link: "/#", icon:"https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/Group_11_h4ym2z.png" },
+
+  ];
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return `${text.slice(0, maxLength)}...`;
+    }
+    return text;
+  };
+  return (
+    <div>
+      <div className="fixed top-0 left-0">
+        <div className=" transition-all ease-in-out duration-300  border-r-2 border-gray-200 flex flex-col items-center h-screen w-[18rem]">
+          <div className="py-9 w-full bg-[#0018A8] h-screen ">
+
+            <div className="flex justify-center items-center gap-3 ">
+              <div>
+                <Image src="https://res.cloudinary.com/phantom1245/image/upload/v1681638954/db%20bank/safari-pinned-tab_1_conyqm.png" alt="cloud icon" width={40} height={40} />
+              </div>
+            </div>
+
+            <div className="py-20">
+              <ul className="w-full flex flex-col">
+                {links.map((link) => (
+                  <li
+                    key={link.name}
+                    className="w-full  duration-300  text-white flex items-center rounded-md font-manrope  justify-between px-8 py-4 text-md capitalize font-bold"
+                  >
+                    <Link
+                      href={link.link}
+                      
+                      className=""
+                    >
+                      <div className="flex items-center gap-3">
+                        <Image src={link.icon} alt={link.name} width={20} height={20} />
+                        <span title={link.name}>
+                          <i className="far fa-user"></i>{" "}
+                          {truncateText(link.name, 17)}
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,66 @@
+import SideNav from '@/components/common/sideNav'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+    <div>
+      <div className="flex flex-row ">
+        <section className=" sticky top-0 left-0 z-[222]">
+          <SideNav />
+        </section>
+        <main className="w-full h-screen z-[99] bg-gray-100">
+          <section className="w-full pl-[20rem] pr-10 pt-10 ">
+           <div>
+              <h1 className='font-bold text-[#000000] pb-8 text-xl font-manrope'>Good Morning Tobias Pickett</h1>
+           </div>
+           <div className='flex gap-3 rounded-lg w-full py-4 px-7  bg-white'>
+              <div className='text-[#0018A8] font-semibold font-manrope'>Home / </div>
+              <div className='text-[#0018A8] font-semibold font-manrope'>Dashbaord /</div>
+              <div className=' font-semibold font-manrope'>Dashboards</div>
+           </div>
+           <div className='flex w-full gap-5 pt-10'>
+            <div className='w-[65%] h-[14rem] mt-[2rem] bg-white  flex rounded-2xl  overflow-hidden '>
+              <div className='w-1/2 flex flex-col justify-center pl-8 items-start'>
+                <div className='text-[#000] text-xl font-semibold font-manrope'>Total Balance</div>
+                <div className='flex gap-4 pt-2'>
+                  <div>
+                    <button className='bg-[#0018A8] py-3 rounded-full text-white w-[7rem] '>Deposit</button>
+                  </div>
+                  <div>
+                    <button className='border-[#0018A8] border-2 rounded-full bg-transparent text-[#0018A8] py-3 w-[7rem] '>Deposit</button>
+                  </div>
+                </div>
+              </div>
+              <div className='w-1/2 overflow-hidden  rounded-r-2xl bg-[#0018A8] rounded-l-full flex flex-col justify-center items-start'>
+                <div className="text-[2.8rem]  font-extrabold font-manrope text-white pl-4">€8,500,000.00</div>
+                <div className='font-light flex text-white pl-5 font-manrope'>ACCOUNT BALANCE</div>
+              </div>
+            </div>
+            <div className='w-[25%] flex flex-col gap-3 pl-4'>
+              <div className='flex flex-col w-full h-[8rem] bg-white  justify-center items-center rounded-xl'>
+                <h1 className='pt-3 text-[#000] font-manrope font-semibold '>
+                  Book Balance 
+                </h1>
+                <h3 className='text-[#0018A8] text-3xl font-manrope font-semibold'>
+                  €0.00
+                </h3>
+              </div>
+              <div className='flex flex-col w-full h-[8rem] bg-white  justify-center items-center rounded-xl'>
+                <h1 className='pt-3 text-[#000] font-manrope font-semibold '>
+                  Available Balance 
+                </h1>
+                <h3 className='text-[#0018A8] text-3xl font-manrope font-semibold'>
+                 €8,500,000.00
+                </h3>
+              </div>
+            </div>
+           </div>
+          </section>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    </div>
     </main>
   )
 }
